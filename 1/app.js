7a4c558d07269fc4d8acbc9e69a7064585aa9d85
Chris Amaya <amayarivasc@gmail.com>
2020-08-14 08:34:10 -0500

    ADD: first commit

---
+++ 1/app.js
@@ -0,0 +1,11 @@
+const result = document.getElementById('result');
+const text = document.getElementById('text');
+const generate = document.getElementById('generate');
+
+generate.addEventListener('click', () => {
+
+    result.innerText = text.value;
+
+})
+
+generate.addEventListener()
